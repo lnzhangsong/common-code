3c5aed2b186cbc2841795531eac4c071c945cf0b
lnzhangsong <lnzhangsong@163.com>
2022-01-04 13:11:13 +0800

    完成 curry

--- basic/curry.js
+++ basic/curry.js
@@ -6,22 +6,32 @@
  */
 
 function curry (fn, args) {
+  // get the number of formal arguments
   let len = fn.length
+  // get actual arguments, if it is not provided then use empty array
   args = args || [];
-  console.log(fn, args);
-
+  // return a function that will be called when the 
+  // number of arguments is equal to the number of formal arguments
   return function () {
+    // like-array to array
+    // concat the arguments with the arguments that are passed to the function
     let _args = args.concat(Array.prototype.slice.call(arguments));
+    console.log(_args, arguments, len);
     if (_args.length >= len) {
+      // call the original function with the arguments
       return fn.apply(this, _args);
     } else {
+      // return a new function that will be called when the
       return curry(fn, _args);
     }
   };
 }
 
-curry(function (a, b, c) {
-  console.log(a, b, c);
-})(1)(2)(3);
+var fn = curry(function (a, b, c) {
+	console.log([a, b, c], '\n');
+});
 
-
+fn("a", "b", "c"); // ["a", "b", "c"]
+fn("a", "b")("c"); // ["a", "b", "c"]
+fn("a")("b")("c"); // ["a", "b", "c"]
+fn("a")("b", "c"); // ["a", "b", "c"]
